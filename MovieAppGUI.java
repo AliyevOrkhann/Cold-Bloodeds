ea3494f580c26a7e2c954fcd4988520eb5d39654
Babek Aliyev <baliyev18013@ada.edu.az>
2023-12-09 23:47:22 +0400

    Adjusting user's home page

--- MovieAppGUI.java
+++ MovieAppGUI.java
@@ -2,68 +2,26 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.List;
 
 public class MovieAppGUI {
-    private JFrame frame;
-    private MovieDatabase movieDatabase;
+    private static JFrame frame;
     private static JTabbedPane tabbedPane;
     private static User currentUser;
-    private JPanel movieListPanel;
-    private JPanel watchlistPanel;
-    private JPanel detailsPanel;
 
     public MovieAppGUI(User user) {
         MovieAppGUI.currentUser = user;
-        this.movieDatabase = new MovieDatabase();
         initialize();
     }
 
     private void initialize() {
-        frame = new JFrame("Movie Database");
+        frame = new JFrame("ColdBloodex");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 600);
 
         tabbedPane = new JTabbedPane();
 
         JPanel userPanel = createUserPanel();
-        tabbedPane.addTab("User", userPanel);
-
-        JPanel tabPanel = new JPanel();
-        tabPanel.setLayout(new BorderLayout());
-
-        movieListPanel = new JPanel(new GridLayout(0,1));
-        watchlistPanel = new JPanel(new GridLayout(0,1));
-        detailsPanel = new JPanel(new GridLayout(0,1));
-
-
-        List<Movie> movies = movieDatabase.getMovies();
-        for (Movie movie : movies) {
-            JButton movieButton = new JButton(movie.getTitle());
-            movieButton.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    showMovieDetails(movie);
-                }
-            });
-            movieListPanel.add(movieButton);
-        }
-
-        JScrollPane movieListScrollPane = new JScrollPane(movieListPanel);
-        JScrollPane watchScrollPane = new JScrollPane(watchlistPanel);
-        JScrollPane detailsScrollPane = new JScrollPane(detailsPanel);
-
-        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, movieListScrollPane, detailsScrollPane);
-        splitPane.setResizeWeight(0.5);
-
-        JSplitPane rightSplitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, splitPane, watchScrollPane);
-        rightSplitPane.setResizeWeight(0.8);
-
-        tabPanel.add(movieListPanel, BorderLayout.NORTH);
-        tabPanel.add(watchlistPanel, BorderLayout.EAST);
-        tabPanel.add(detailsPanel, BorderLayout.CENTER);
-
-        tabbedPane.addTab("Every", tabPanel);
+        tabbedPane.addTab("Login", userPanel);
 
         frame.getContentPane().add(tabbedPane);
         frame.setVisible(true);
@@ -92,7 +50,6 @@ public class MovieAppGUI {
                 String password = passwordField.getText();
 
                 if (User.register(username, password)) {
-
                     usernameField.setText("");
                     passwordField.setText("");
                 }
@@ -106,69 +63,12 @@ public class MovieAppGUI {
                 String password = passwordField.getText();
 
                 if (User.login(username, password)) {
-
-                    usernameField.setText("");
-                    passwordField.setText("");
-                    new UserLoginWindow();
+                    frame.dispose();
+                    new UserLoginWindow(currentUser);
                 }
             }
         });
         return panel;
-    }
-
-    private void showMovieDetails(Movie movie) {
-        detailsPanel.removeAll();
-
-        JLabel titleLabel = new JLabel("Title: " + movie.getTitle());
-        JLabel directorLabel = new JLabel("Director: " + movie.getDirector());
-        JLabel releaseLabel = new JLabel("Release Year: " + movie.getReleaseYear());
-        JLabel runningTimeLabel = new JLabel("Running Time: " + movie.getRunningTime() + " minutes");
-
-        JButton addToWatchlistButton = new JButton("Add to Watchlist");
-        addToWatchlistButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                currentUser.addToWatchList(movie);
-                updateWatchlistPanel();
-            }
-        });
-
-        JButton removeFromWatchlistButton = new JButton("Remove from Watchlist");
-        removeFromWatchlistButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                currentUser.removeFromWatchlist(movie);
-                updateWatchlistPanel();
-            }
-        });
-
-        detailsPanel.add(titleLabel);
-        detailsPanel.add(directorLabel);
-        detailsPanel.add(releaseLabel);
-        detailsPanel.add(runningTimeLabel);
-        detailsPanel.add(addToWatchlistButton);
-        detailsPanel.add(removeFromWatchlistButton);
-
-        detailsPanel.revalidate();
-        detailsPanel.repaint();
-    }
-
-    private void updateWatchlistPanel() {
-        watchlistPanel.removeAll();
-        List<Movie> watchList = currentUser.getWatchList();
-        for(Movie movie : watchList) {
-            JButton movieButton = new JButton(movie.getTitle());
-            movieButton.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    currentUser.removeFromWatchlist(movie);
-                    updateWatchlistPanel();
-                }
-            });
-            watchlistPanel.add(movieButton);
-        }
-        watchlistPanel.revalidate();
-        watchlistPanel.repaint();
     }
 
     public static void main(String[] args) {
--- UserLoginWindow.java
+++ UserLoginWindow.java
@@ -1,13 +1,126 @@
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
 
-public class UserLoginWindow {
-    JFrame frame = new JFrame("Welcome!");
+public class UserLoginWindow{
+    private JPanel movieListPanel;
+    private JPanel watchlistPanel;
+    private JPanel detailsPanel;
+    private MovieDatabase movieDatabase;
 
-    UserLoginWindow(){
+    UserLoginWindow(User currentUser){
+        JFrame frame = new JFrame("Home Page");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 600);
+
+        this.movieDatabase = new MovieDatabase();
+
+        JPanel tabPanel = new JPanel();
+        tabPanel.setLayout(new BorderLayout());
+
+        movieListPanel = new JPanel(new GridLayout(0,1));
+        watchlistPanel = new JPanel(new GridLayout(0,1));
+        detailsPanel = new JPanel(new GridLayout(0,1));
+
+        List<Movie> movies = movieDatabase.getMovies();
+        for (Movie movie : movies) {
+            JButton movieButton = new JButton(movie.getTitle());
+            movieButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    showMovieDetails(movie, currentUser);
+                }
+            });
+            movieListPanel.add(movieButton);
+            movieButton.setFocusable(true);
+        }
+
+
+
+        JScrollPane movieListScrollPane = new JScrollPane(movieListPanel);
+        JScrollPane watchScrollPane = new JScrollPane(watchlistPanel);
+        JScrollPane detailsScrollPane = new JScrollPane(detailsPanel);
+
+        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, movieListScrollPane, detailsScrollPane);
+        splitPane.setResizeWeight(0.5);
+
+        JSplitPane rightSplitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, splitPane, watchScrollPane);
+        rightSplitPane.setResizeWeight(0.8);
+
+        tabPanel.add(movieListPanel, BorderLayout.NORTH);
+        tabPanel.add(watchlistPanel, BorderLayout.EAST);
+        tabPanel.add(detailsPanel, BorderLayout.CENTER);
+
+        frame.add(tabPanel);
+
         frame.setVisible(true);
     }
 
+    private void updateWatchlistPanel(User currentUser) {
+        watchlistPanel.removeAll();
+        List<Movie> watchList = currentUser.getWatchList();
+        for(Movie movie : watchList) {
+            JButton movieButton = new JButton(movie.getTitle());
+            movieButton.setFocusable(true);
+            movieButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    currentUser.removeFromWatchlist(movie);
+                    updateWatchlistPanel(currentUser);
+                }
+            });
+            watchlistPanel.add(movieButton);
+        }
+        watchlistPanel.revalidate();
+        watchlistPanel.repaint();
+    }
+
+    private void showMovieDetails(Movie movie, User currentUser) {
+        detailsPanel.removeAll();
+
+        JLabel titleLabel = new JLabel("Title: " + movie.getTitle());
+        JLabel directorLabel = new JLabel("Director: " + movie.getDirector());
+        JLabel releaseLabel = new JLabel("Release Year: " + movie.getReleaseYear());
+        JLabel runningTimeLabel = new JLabel("Running Time: " + movie.getRunningTime() + " minutes");
+
+        JButton addToWatchlistButton = new JButton("Add to Watchlist");
+        addToWatchlistButton.setFocusable(true);
+        addToWatchlistButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                currentUser.addToWatchList(movie);
+                updateWatchlistPanel(currentUser);
+            }
+        });
+
+        JButton removeFromWatchlistButton = new JButton("Remove from Watchlist");
+        removeFromWatchlistButton.setFocusable(true);
+        removeFromWatchlistButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                currentUser.removeFromWatchlist(movie);
+                updateWatchlistPanel(currentUser);
+            }
+        });
+
+        detailsPanel.add(titleLabel);
+        detailsPanel.add(directorLabel);
+        detailsPanel.add(releaseLabel);
+        detailsPanel.add(runningTimeLabel);
+        detailsPanel.add(addToWatchlistButton);
+        detailsPanel.add(removeFromWatchlistButton);
+
+        detailsPanel.revalidate();
+        detailsPanel.repaint();
+    }
+
 }
-    
